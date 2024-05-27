52250363d73b4360e7cb4d6d891347817f16d72c
Aliza khan <155885247+alizak-13@users.noreply.github.com>
2024-05-27 10:04:51 -0700

    Add files via upload
    A scientific calculator with a BMI calculator which can perform addition
    Subtraction
    Multiplication 
    Division
    Modulus and
    Exponentiation

---
+++ index.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    message: "Enter firstnumber",
+    type: "number",
+    name: "Firstnumber",
+  },
+  { message: "Enter secondnumber", type: "number", name: "Secondnumber" },
+  {
+    message: "Select one of the operators to perform operations",
+    type: "list",
+    name: "operator",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Multiplication",
+      "Division",
+      "Modulus",
+      "Exponentiation",
+      "BMI",
+    ],
+  },
+]);
+if (answer.operator === "Addition") {
+  console.log(answer.Firstnumber + answer.Secondnumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.Firstnumber - answer.Secondnumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.Firstnumber * answer.Secondnumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.Firstnumber / answer.Secondnumber);
+} else if (answer.operator === "Modulus") {
+  console.log(answer.Firstnumber % answer.Secondnumber);
+} else if (answer.operator === "Exponentiation") {
+  console.log(answer.Firstnumber ** answer.Secondnumber);
+} else if (answer.operator === "BMI") {
+  console.log(answer.Firstnumber / (answer.Secondnumber * answer.Secondnumber));
+} else {
+  console.log("Please select a valid operator");
+}
